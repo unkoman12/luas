fdbfd3cd38c4338a2f70be8f25c187084804c78a
unkoman12 <takbowtakbow+githubbb@gmail.com>
2024-11-13 14:35:01 +0900

    Delete SC.lua

--- SC.lua
+++
@@ -1,8 +0,0 @@
-gg.alert("偽物大量発生したせいでスクリプトは配布終了しましたよ？ｗｗｗ")
-function Main()
-    Menu = gg.choice({ 
-"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
-    }, nil, '乞食しね（笑）')
-    if Menu == 1 then hanei1() end
-    YUNI = -1
-end
